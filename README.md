02c67143d132f9c262ac7376d50d7d659d6a9e57
Alec Dilanchian <alec.dilanchian@gmail.com>
2019-07-30 06:54:55 -0700

    Updating to mono repo && adding native sub folders && adding gitignore

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
-# Grüvee-RN
+# Grüvee
 
-This repo will house all our React Native Componenets.
+This repo houses our iOS/Android native shells and React Native components. We need the shells as subfolders in our React Native project in order to have 
+the "native shell" experience.
---
+++ ios/Gruvee/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Gruvee
+//
+//  Created by Alec Dilanchian on 7/30/19.
+//  Copyright © 2019 Pixelogic LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+}
+
