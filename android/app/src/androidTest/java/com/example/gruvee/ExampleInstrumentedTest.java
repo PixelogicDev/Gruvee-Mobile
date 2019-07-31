560977ccbaeea3135fa5e2a5483efee44d90f98a
Alec Dilanchian <alec.dilanchian@gmail.com>
2019-07-31 05:57:47 -0700

    Adding finalized initial android project

--- android/app/src/androidTest/java/com/example/gruvee/ExampleInstrumentedTest.java
+++ android/app/src/androidTest/java/com/example/gruvee/ExampleInstrumentedTest.java
@@ -1,6 +1,7 @@
 package com.example.gruvee;
 
 import android.content.Context;
+
 import androidx.test.InstrumentationRegistry;
 import androidx.test.runner.AndroidJUnit4;
 
--- android/app/src/main/java/com/example/gruvee/MainActivity.java
+++ android/app/src/main/java/com/example/gruvee/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.gruvee;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
