c2236701a368b0f131d8bcd1e4aa7fe444943e2f
Alec Dilanchian <alec.dilanchian@gmail.com>
2019-10-31 07:25:44 -0700

    Adding new comment class && moving mock data to mock folder

---
+++ Mock/mockSongComments.js
@@ -0,0 +1,22 @@
+export default [
+    {
+        id: 'Comment1',
+        timestamp: new Date(),
+        message: 'Hi, my name is Alec, and I thought this song was gooooooood.',
+        sender: 'adilanchian',
+    },
+    {
+        id: 'Comment2',
+        timestamp: new Date(),
+        message:
+            'Hi, my name is JOSMO, and I thought this song was gooooooood.',
+        sender: 'JOSMO',
+    },
+    {
+        id: 'Comment3',
+        timestamp: new Date(),
+        message:
+            'Hi, my name is H3llo, and I thought this song was gooooooood.',
+        sender: 'H3llo',
+    },
+]
