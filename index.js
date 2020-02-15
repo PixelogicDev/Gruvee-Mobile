f3fa73aa4657eac519fba268e60a93232d9987f1
Alec Dilanchian <alec.dilanchian@gmail.com>
2020-02-15 13:29:02 -0800

    Adding Pixel Redemption comment

--- index.js
+++ index.js
@@ -3,9 +3,10 @@
  * BackeyM - "Author: @backey Copyright© 2020" (02/13/20)
  * syszen - "inflation will occur - pixelogicdev" (02/13/20)
  * syszen - "for future proof reference 10000 active users = gruvee tattoo" (02/13/20)
+ * TheDkbay - "Gib name" (1/22/2020)
+ * Stacking - "He had no arms or legs. He couldn't see, hear, or speak. This is how he led a nation." (01/27/20)
+ * sillyonly - "this is still high! I would love to get a discount on this object! please!" (02/15/20)
  * @format
- TheDkbay - "Gib name" (1/22/2020)
- Stacking - "He had no arms or legs. He couldn't see, hear, or speak. This is how he led a nation." (01/27/20)
  */
 
 import { Navigation } from 'react-native-navigation'
