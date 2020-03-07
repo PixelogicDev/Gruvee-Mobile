0275e13fb72fd2c19720652fd0f17517ea7ccd31
Alec Dilanchian <alec.dilanchian@gmail.com>
2020-03-07 01:09:43 -0800

    Tweaking promises for spotify auth && adding android deep link suppotrt

--- Components/Auth/Auth.js
+++ Components/Auth/Auth.js
@@ -13,17 +13,38 @@ import { firebase } from '@react-native-firebase/auth'
 import * as StyleConstants from '@StyleConstants'
 import { SetInitialUserData } from 'Gruvee/Redux/Actions/User/UserActions'
 import React, { useEffect } from 'react'
-import { Linking, StyleSheet, Text, View } from 'react-native'
+import { Linking, StyleSheet, Text, View, Platform } from 'react-native'
+import AsyncStorage from '@react-native-community/async-storage'
+
 // Redux
 import { connect } from 'react-redux'
 import { HandleSpotifyDeepLink } from './Actions/SpotifyActions'
 import SocialButtons from './Buttons'
 
+const DEEP_LINK_IN_PROGRESS_FLAG = '@Deep_Link_In_Progress'
+
 // no_neon_one - "Btw I Use Arch!" (02/17/20)
 const Auth = ({ setInitialUserData }) => {
     useEffect(() => {
+        // firebase.auth().signOut()
+        // AsyncStorage.setItem('@Deep_Link_In_Progress', 'false')
         // Deep Link Handler
-        Linking.addEventListener('url', handleOpenUrl)
+        if (Platform.OS === 'android') {
+            // Android instaniates multiple activites with deep links
+            // To combat insane calls to out handler, set a flag here to stop it if it's already working
+            AsyncStorage.getItem(DEEP_LINK_IN_PROGRESS_FLAG).then(value => {
+                if (value === null || value === 'false') {
+                    // First time running this
+                    Linking.getInitialURL().then(url => {
+                        if (url !== null) {
+                            handleOpenUrl({ url })
+                        }
+                    })
+                }
+            })
+        } else {
+            Linking.addEventListener('url', handleOpenUrl)
+        }
 
         // Apple Authentication Handler
         /* if (appleAuth.isSupported) {
@@ -45,6 +66,7 @@ const Auth = ({ setInitialUserData }) => {
     const handleOpenUrl = async event => {
         try {
             let newUserObj = {}
+            AsyncStorage.setItem('@Deep_Link_In_Progress', 'true')
 
             // Check to see what platform this is coming from
             if (event.url.includes('spotify_auth')) {
@@ -56,18 +78,15 @@ const Auth = ({ setInitialUserData }) => {
             // After auth, we should always set initial user data and sign via firebase
             setInitialUserData(newUserObj.user, newUserObj.jwt)
 
-            const userCreds = await firebase
-                .auth()
-                .signInWithCustomToken(newUserObj.jwt)
-            if (userCreds !== null) {
-                console.log('Have user creds, pushing to playlists view.')
+            // Passes back userCreds, not sure what to do with those yet
+            firebase.auth().signInWithCustomToken(newUserObj.jwt)
 
-                // Navigate to Playlists View
-                pushToPlaylistsList()
-            }
+            AsyncStorage.setItem('@Deep_Link_In_Progress', 'false')
         } catch (error) {
             // TODO: Handle Error
             console.warn(error)
+
+            AsyncStorage.setItem('@Deep_Link_In_Progress', 'false')
         }
     }
 
@@ -83,21 +102,6 @@ const Auth = ({ setInitialUserData }) => {
             <View style={styles.ButtonContainer}>{SocialButtons}</View>
         </View>
     )
-}
-
-// Helpers
-const pushToPlaylistsList = () => {
-    Navigation.push(NavigationConstants.STACK_ID, {
-        component: {
-            name: NavigationConstants.PLAYLIST_NAV_NAME,
-            // LilCazza - 'God Save the Queen of Australia' (03/05/20)
-            options: {
-                topBar: {
-                    visible: false,
-                },
-            },
-        },
-    })
 }
 
 // Styles
--- Service/Spotify/Endpoints.js
+++ Service/Spotify/Endpoints.js
@@ -10,37 +10,33 @@ import { GET_API_TOKEN } from './EndpointConstants'
 const baseHostName =
     ENVIRONMENT === 'PROD'
         ? 'https://us-central1-gruvee-3b7c4.cloudfunctions.net'
-        : 'http://localhost:8080'
+        : 'http://192.168.1.12:8080'
 
 // POST: API Token Request
-export const GetApiToken = async code => {
-    try {
-        const credsB64 = Buffer.from(
-            `${SpotifyCreds.clientId}:${SpotifyCreds.clientSecret}`
-        ).toString('base64')
+export const GetApiToken = code => {
+    const credsB64 = Buffer.from(
+        `${SpotifyCreds.clientId}:${SpotifyCreds.clientSecret}`
+    ).toString('base64')
 
-        const params = new URLSearchParams()
-        params.append('grant_type', 'authorization_code')
-        params.append('code', code)
-        params.append('redirect_uri', SpotifyCreds.redirectUri)
+    const params = new URLSearchParams()
+    params.append('grant_type', 'authorization_code')
+    params.append('code', code)
+    params.append('redirect_uri', SpotifyCreds.redirectUri)
 
-        const headers = {
-            Authorization: `Basic ${credsB64}`,
-            'content-type': 'application/x-www-form-urlencoded',
-        }
-
-        const options = {
-            method: 'POST',
-            headers,
-            data: params,
-            url: GET_API_TOKEN,
-        }
-
-        const response = await axios(options)
-        return Promise.resolve(response.data)
-    } catch (error) {
-        return Promise.reject(error)
+    const headers = {
+        Authorization: `Basic ${credsB64}`,
+        'content-type': 'application/x-www-form-urlencoded',
     }
+
+    const options = {
+        method: 'POST',
+        url: GET_API_TOKEN,
+        headers,
+        data: params,
+    }
+
+    // This already returns a promise
+    return axios(options)
 }
 
 // POST: API Token Refresh
@@ -49,48 +45,33 @@ export const RefreshApiToken = async () => {
 }
 
 // POST: Authorize Spotify User
-export const AuthorizeUser = async token => {
-    let result
-    try {
-        const options = {
-            method: 'POST',
-            data: {
-                token,
-            },
-            url: `${baseHostName}/authorizeWithSpotify`,
-        }
-
-        const response = await axios(options)
-        result = Promise.resolve(response.data)
-    } catch (error) {
-        result = Promise.reject(error.response)
+export const AuthorizeUser = token => {
+    const options = {
+        method: 'POST',
+        url: `${baseHostName}/authorizeWithSpotify`,
+        data: {
+            token,
+        },
     }
 
-    return result
+    // This already returns a promise
+    return axios(options)
 }
 
 // KociQQ: Comment - "Found on StackOverflow, don't trust this code" (02/25/20)
 // GET: Get Custom Firebase Token
 export const GetCustomFirebaseToken = async uid => {
-    let result
-
-    try {
-        const headers = {
-            'User-Type': 'gruvee-mobile',
-        }
-
-        const options = {
-            method: 'POST',
-            headers,
-            url: `${baseHostName}/generateCustomToken`,
-            data: { uid },
-        }
-
-        const response = await axios(options)
-        result = Promise.resolve(response.data)
-    } catch (error) {
-        result = Promise.reject(error)
+    const headers = {
+        'User-Type': 'gruvee-mobile',
     }
 
-    return result
+    const options = {
+        method: 'POST',
+        url: `${baseHostName}/generateCustomToken`,
+        headers,
+        data: { uid },
+    }
+
+    // This already returns a promise
+    return axios(options)
 }
