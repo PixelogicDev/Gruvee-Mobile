7e9f955860da3d0c45be1574433bfcb50c48aeaa
Alec Dilanchian <alec.dilanchian@gmail.com>
2020-04-08 08:31:23 -0700

    fixing multiple id issue in fetchmembers

--- app/redux/actions/members/DispatchActions.js
+++ app/redux/actions/members/DispatchActions.js
@@ -11,15 +11,23 @@ export const FetchMembers = (membersState, members) => {
     // Get user state playlists, map through IDs, and return
     if (members.length === 0) return membersState
 
+    // If the user is already here, do not try and set again
+    const newMembers = members.filter(member => !membersState.byId[member.id])
+
     // TODO: Think if we want to use reduce vs forEach (O(n^2) vs O(n))
-    const reducedMembers = members.reduce(
+    const reducedMembers = newMembers.reduce(
         (state, currentMember) => {
+            // Members in multiple playlists should not be added multiple times to allIds
+            const allIds =
+                state.allIds.findIndex(id => currentMember.id === id) === -1
+                    ? [...state.allIds, currentMember.id]
+                    : [...state.allIds]
             return {
                 byId: {
                     ...state.byId,
                     [currentMember.id]: currentMember,
                 },
-                allIds: [...state.allIds, currentMember.id],
+                allIds,
             }
         },
         { byId: {}, allIds: [] }
