859f4dd80a4ad48eff731e3a1d2dae7c1140bf58
Alec Dilanchian <alec.dilanchian@gmail.com>
2020-06-17 07:49:02 -0600

    Adding check for iOS 13

--- app/components/Auth/components/Buttons/index.js
+++ app/components/Auth/components/Buttons/index.js
@@ -3,10 +3,16 @@ import { Platform } from 'react-native'
 import AppleAuthButton from './AppleAuthButton'
 import SpotifyAuthButton from './SpotifyAuthButton'
 
-const iosButtons = [
-    <AppleAuthButton key="apple-auth-button" />,
-    <SpotifyAuthButton key="spotify-auth-button" />,
-]
+// Apple Auth is only supported on iOS 13 and up
+// We will be hiding the button if device is not on iOS 13
+const iOSMajorVersion = parseInt(Platform.Version, 10)
+const iosButtons =
+    iOSMajorVersion >= 13
+        ? [
+              <AppleAuthButton key="apple-auth-button" />,
+              <SpotifyAuthButton key="spotify-auth-button" />,
+          ]
+        : [<SpotifyAuthButton key="spotify-auth-button" />]
 const androidButtons = [<SpotifyAuthButton key="spotify-auth-button" />]
 
 export default Platform.select({ ios: iosButtons, android: androidButtons })
