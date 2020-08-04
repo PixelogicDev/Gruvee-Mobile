691c6f22645098519c1d0db3ce4e03a7e07daeb4
Alec Dilanchian <alec.dilanchian@gmail.com>
2020-08-04 08:01:37 -0700

    Adding movies comment && tweaking spotify artwork for song item

--- Movies.js
+++ Movies.js
@@ -65,6 +65,8 @@
 // ForTheWyn - "Maybe. Just maybe my boys can protect the book. Yeah, and maybe I'm a Chinese jet pilot." - Evil Dead III: Army Of Darkness (06/09/20)
 // DR_DinoMight - "Ted, while I agree that, in time, our band will be most triumphant. The truth is, Wyld Stallyns will never be a super band until we have Eddie Van Halen on guitar." - Bill and Ted’s Excellent Adventure (06/09/20)
 // Moov_ - "So, yeah, I'm a kid. And I'm also a goofball, and a wingnut, and a knucklehead McSpazatron! But most of all, I'm... I'm I'm a Goofy Goober!" SpongeBob Squarepants Movie (07/08/20)
+// Artandor - "It's not who I am underneath, but what I do, that defines me" Batman: Begins (07/28/20)
+// wrobirson - Why do I keep doing the same thing and expecting a different result? Isn't that the definition of insanity? Castlevania (Netflix version) (07/31/20)
 
 // -- SUDOKU -- //
 // OnePocketPimp: |8 _ _ | _ _ _ | _ _ _ |
--- app/components/SongList/components/SongItemDetail.js
+++ app/components/SongList/components/SongItemDetail.js
@@ -69,9 +69,7 @@ const getAlbumArtwork = (songData, platform) => {
         case 'apple':
             return { uri: songData.apple.images.url.replace('{w}', '160').replace('{h}', '160') }
         case 'spotify':
-            return songData.images && songData.images.length
-                ? { uri: songData.images[0].url }
-                : null
+            return songData.spotify.images.length ? { uri: songData.spotify.images[0].url } : null
         case 'youtube':
             console.log('Youtube not supported yet!')
             break
